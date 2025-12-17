b3b66b2f80e2ad6cb583acb7af5d93478c8571e3
afip-create <262602484+afip-create@users.noreply.github.com>
2025-12-17 17:32:38 +0000

    Added ErrorOrNil method to WebResponse type

--- helpers.go
+++ helpers.go
@@ -1,7 +1,12 @@
-
 type WebResponse struct {
     Success bool        `json:"success"`
     Data    interface{} `json:"data,omitempty"`
-    Error   string      `json:"error,omitempty"`
+    Error   *string     `json:"error,omitempty"`
 }
 
+func (r *WebResponse) ErrorOrNil() error {
+    if r.Error != nil {
+        return fmt.Errorf("error: %s", *r.Error)
+    }
+    return nil
+}
